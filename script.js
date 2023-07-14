6439bc5eac7ad050fe4d1b30d8c6c087a6644082
PSBahia <paulo.rpba@gmail.com>
2023-07-14 15:47:19 -0300

    adicao da pagina biografia

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ function toggleMode(){
   const img = document.querySelector("#profile img") 
   
   if (html.classList.contains('light')){
-    img.setAttribute("src", "./assets/avatar_perfil.png")
+    img.setAttribute(
+      "src",
+      "https://media.licdn.com/dms/image/D4D35AQGmyMdvQhe2WA/profile-framedphoto-shrink_200_200/0/1688071936798?e=1689818400&v=beta&t=p3KzQ2-kQ3sFuiTDfZe_eKfLSRTfVH52gD4xkxgPS9w"
+    )
     img.setAttribute("alt", "Perfil")
   }else{
     img.setAttribute(
       "src",
-      "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Picture.png"
+      "https://media.licdn.com/dms/image/D4D35AQGmyMdvQhe2WA/profile-framedphoto-shrink_200_200/0/1688071936798?e=1689818400&v=beta&t=p3KzQ2-kQ3sFuiTDfZe_eKfLSRTfVH52gD4xkxgPS9w"
     )
+    img.setAttribute("alt", "Perfil")
   }
   
 }
